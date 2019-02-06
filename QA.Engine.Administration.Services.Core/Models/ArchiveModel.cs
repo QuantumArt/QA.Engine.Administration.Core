ae37333ed41d7ad82f10634bf9ad9e862e71d735
ARTQ\NeronovA <NeronovA@quantumart.ru>
2019-02-06 14:33:46 +0300

    fix commit (+files)

---
+++ QA.Engine.Administration.Services.Core/Models/ArchiveModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace QA.Engine.Administration.Services.Core.Models
+{
+    public class ArchiveModel
+    {
+        public List<PageModel> Pages { get; set; }
+        public List<WidgetModel> Widgets { get; set; }
+    }
+}
