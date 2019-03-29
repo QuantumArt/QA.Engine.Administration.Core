abe5e36bdfbf644673f9fc5991e30af729e88586
Michael <vozzic@gmail.com>
2019-03-29 19:00:41 +0300

    - fix showID

--- QA.Engine.Administration.WebApp.Core/ClientApp/components/TreeStructure/NodeLabel.tsx
+++ QA.Engine.Administration.WebApp.Core/ClientApp/components/TreeStructure/NodeLabel.tsx
@@ -21,7 +21,7 @@ export default class NodeLabel extends React.Component<Props> {
         if (!tree) {
             return null;
         }
-        if (tree.showIDs && node.contextMenuType > 0) {
+        if (tree.showIDs && node.id > 0) {
             return <span className="bp3-tree-node-label">{`${node.title} - ${node.id}`}</span>;
         }
         if (tree.searchActive && tree.showPath) {
