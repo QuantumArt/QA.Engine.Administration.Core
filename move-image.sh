9764bdc16a8f112e653e57179592c1b4f5305b31
Pavel Celut <CelutP@gmail.com>
2022-10-06 15:21:17 +0300

    Fix move script

--- move-image.sh
+++ move-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull '||registry-source||/||image-source||:||version||' && \
-docker tag '||registry-source||/||image-source||:||version||' '||registry-receiver||/||image-receiver||:||version||' && \
-docker push '||registry-receiver||/||image-receiver||:||version||'
+docker tag '||registry-source||/||image-source||:||version||' '||registry-destination||/||image-destination||:||version||' && \
+docker push '||registry-destination||/||image-destination||:||version||'
